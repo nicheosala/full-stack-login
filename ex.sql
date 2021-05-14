133c7cf839184f123b9207b3bc72906c4f347841
nicheosala <48255219+nicheosala@users.noreply.github.com>
2021-05-14 19:39:24 +0200

    Useless files removed.

--- ex.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `nodelogin`;
-USE `nodelogin`;
-CREATE TABLE IF NOT EXISTS `accounts` (`id` INT AUTO_INCREMENT PRIMARY KEY, `username` varchar(50) NOT NULL, `password` varchar(255) NOT NULL);
-
--- INSERT INTO `accounts` (`username`, `password`) VALUES ('test', 'test');
--- postgre.sql
+++
@@ -1,9 +0,0 @@
-create database nodelogin;
-
-\c nodelogin;
-
-create table if not exists accounts (
-    id serial primary key,
-    username varchar(255) not null,
-    password varchar(255) not null
-);
